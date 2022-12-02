a117b72620b4c795d985727a53319545da211d12
Crack-way <rupeshsunuwar2019@gmail.com>
2022-12-02 23:36:14 +0545

    Implementing the create Employee method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,26 @@
 import config.DBConnection;
+import enums.AccountType;
+import model.account.Account;
+import model.employe.Employee;
 import model.student.Students;
 import service.student.StudentService;
 import service.student.StudentServiceImpl;
+import model.employe.Employee;
+import service.employee.EmployeeService;
+import service.employee.EmployeeServiceImpl;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.List;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.*;
+import java.util.stream.Collectors;
+
+
 
 public class Main {
 
@@ -24,7 +38,7 @@ public class Main {
 //        BlobService blobService = new BlobService();
 //        blobService.insertBlob();
 //
-        StudentService studentService = new StudentServiceImpl();
+//        StudentService studentService = new StudentServiceImpl();
 //        Students students = studentService.getStudentById(2L);
 //        System.out.println(students.toString());
 
@@ -34,13 +48,118 @@ public class Main {
 //            System.out.println(students1.toString());
 //        }
 
-        Students studentTobeUpdated = new Students();
-        studentTobeUpdated.setId(1L);
-        studentTobeUpdated.setName("Suresh Lama Lama");
-        studentTobeUpdated.setRollNumber("1010ZZ");
-        studentTobeUpdated.setGrade("7th Semeter");
-        studentService.updateStudent(studentTobeUpdated);
-        studentService.deleteStudentById(1L);
+//        Students studentTobeUpdated = new Students();
+//        studentTobeUpdated.setId(1L);
+//        studentTobeUpdated.setName("Suresh Lama Lama");
+//        studentTobeUpdated.setRollNumber("1010ZZ");
+//        studentTobeUpdated.setGrade("7th Semeter");
+//        studentService.updateStudent(studentTobeUpdated);
+//        studentService.deleteStudentById(1L);
+
+        /*List<String> stringList = new ArrayList<String>();
+        stringList.add("A");
+        stringList.add("b");
+        stringList.add("c");
+        stringList.add("d");
+        stringList.add("e");
+        stringList.add("f");
+
+        stringList.stream().forEach( (x) -> System.out.println(x));
+
+
+        List<Integer> integerList = new ArrayList<>();
+        integerList.add(1);
+        integerList.add(2);
+        integerList.add(3);
+        integerList.add(4);
+        integerList.add(5);
+        integerList.add(6);
+        integerList.add(7);
+
+
+        List<String> integerToStringList = integerList.stream().map( x -> String.valueOf(x) ).collect(Collectors.toList());
+
+        integerList.stream().forEach( x-> {
+            System.out.println(x);
+            if(x instanceof Integer){
+                System.out.println(x+" is a int type");
+            }
+        });
+
+
+        List<Employee> employeeList = new ArrayList<>();
+
+        employeeList.add( new Employee(1, "Ram", new Timestamp(new Date().getTime())) );
+        employeeList.add( new Employee(2, "Sam", new Timestamp(new Date().getTime())) );
+        employeeList.add( new Employee(3, "vim", new Timestamp(new Date().getTime())) );
+        employeeList.add( new Employee(4, "Ubuntu", new Timestamp(new Date().getTime())) );
+        employeeList.add( new Employee(5, "Duke", new Timestamp(new Date().getTime())) );
+        employeeList.add( new Employee(6, "Lux", new Timestamp(new Date().getTime())) );
+        employeeList.add( new Employee(7, "Gin", new Timestamp(new Date().getTime())) );
+
+        List<String> nameList = new ArrayList<>();
+
+        for(Employee employee : employeeList){
+            nameList.add(employee.getName());
+        }
+
+
+
+        List<String> nameList2 = employeeList.stream()
+                .filter(x-> x.getName().equalsIgnoreCase("lux") )
+                .map( employee -> employee.getName())
+                .collect(Collectors.toList());
+
+        System.out.println();*/
+
+        /*Integer a = 128;
+        Integer b = 128;
+
+        if(a.equals(b)){
+            System.out.println("a equals to b");
+        }else {
+            System.out.println("a not equals to b");
+        }*/
+
+        /*Account account = new Account(20, AccountType.CHECKING_ACCOUNT, 104);
+        Account account2 = new Account(20, AccountType.CHECKING_ACCOUNT, 104);
+
+
+        if(account.equals(account2)){
+            System.out.println("account equals to account2");
+        }else {
+            System.out.println("account not equals to account2");
+        }
+
+        Set<Account> accountSet = new HashSet<>();
+        accountSet.add(account);
+        accountSet.add(account2);
+
+
+        System.out.println();*/
+
+//        String str = "APPLE";
+//        String str2 = new String("APPLE");
+//        String str3 = "APPLE";
+//
+//        if(str == (str2)){
+//            System.out.println("account equals to account2");
+//        }else {
+//            System.out.println("account not equals to account2");
+//        }
+
+        //Getting the current time stamp.
+
+        final Timestamp timestamp =
+                Timestamp.valueOf(LocalDateTime.of(LocalDate.of(2018, 10, 7), LocalTime.of(8, 45, 0)));
+
+        Employee employee1=new Employee(1,"Rupesh Sunuwar" , timestamp);
+
+        EmployeeServiceImpl obj=  new EmployeeServiceImpl();
+        obj.createEmployee(employee1);
+
+
+
     }
 
 }
--- src/main/java/service/employee/EmployeeServiceImpl.java
+++ src/main/java/service/employee/EmployeeServiceImpl.java
@@ -1,7 +1,42 @@
 package service.employee;
 
-//implement EmployeeService
+import model.employe.Employee;
+
+
+import config.DBConnection;
+import model.employe.Employee;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+//TODO: implement EmployeeService
 public class EmployeeServiceImpl {
+
+   private Connection connection=DBConnection.connect();
+
+
+    public int createEmployee(Employee employee){
+
+        try{
+            PreparedStatement sqlQuery=connection.prepareStatement("INSERT INTO employee"+"(name , hire_date)VALUES (?,?)");
+            sqlQuery.setString(1,employee.getName());
+            sqlQuery.setTimestamp(2,employee.getHireDate());
+            int count=sqlQuery.executeUpdate();
+            System.out.println("Number of data inserted is"+ count);
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+
+        return 0;
+
+    }
+
+
 
 
 }
