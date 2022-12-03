7b3b23cde03c9f0674686986dd69a8b3da54d8fc
Gyalbu Sherpa <gyalbusherpa555@gmail.com>
2022-12-03 16:53:40 +0545

    first change

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -129,15 +129,26 @@ public class Main {
 
         System.out.println();*/
 
-        String str = "APPLE";
-        String str2 = new String("APPLE");
-        String str3 = "APPLE";
+//        String str = "APPLE";
+//        String str2 = new String("APPLE");
+//        String str3 = "APPLE";
+//
+//        if(str == (str2)){
+//            System.out.println("account equals to account2");
+//        }else {
+//            System.out.println("account not equals to account2");
+//        }
 
-        if(str == (str2)){
-            System.out.println("account equals to account2");
-        }else {
-            System.out.println("account not equals to account2");
-        }
+        DBConnection.connect();
+        StudentService s = new StudentServiceImpl();
+
+        Students st = new Students();
+        st.setId(2L);
+        st.setName("don donis me");
+        st.setGrade("third");
+        st.setRollNumber("21");
+
+        s.createStudent(st);
 
     }
 
