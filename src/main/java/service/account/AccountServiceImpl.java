4dc11a27fd7a57aae75746210ad1ac8374377320
siddhartha0 <siddhartha.sunuwar@gmail.com>
2022-12-09 13:32:59 +0545

    First one

--- src/main/java/service/account/AccountServiceImpl.java
+++ src/main/java/service/account/AccountServiceImpl.java
@@ -1,8 +1,58 @@
 package service.account;
+import enums.AccountType;
+import config.DBConnection;
+import service.employee.EmployeeService;
+import service.employee.EmployeeServiceImpl;
+
+import java.sql.*;
 
 //TODO: implement AccountService here
-public class AccountServiceImpl {
+public class AccountServiceImpl implements AccountService {
+    Connection connect;
+    PreparedStatement ps;
+    ResultSet result;
+    final private EmployeeService employee;
 
+    AccountServiceImpl(){
+        employee=new EmployeeServiceImpl();
+        connect=DBConnection.connect();
+    }
+    @Override
+    public void createAccountByType(AccountType accountType, Double startAmount, Integer employeeId) {
 
+        if(accountType.equals(AccountType.CHECKING_ACCOUNT)){
+            employee.createNewChecking(startAmount,employeeId);
+        }
+        else if(accountType.equals(AccountType.SAVING_ACCOUNT)){
+            employee.createNewSavings(startAmount,employeeId);
+        }
+        else if(accountType.equals(AccountType.RETIREMENT_ACCOUNT)){
+            employee.createNewRetirement(startAmount,employeeId);
+        }
+        else{
+            System.out.println("Please select the valid option");
+        }
 
+    }
+
+    @Override
+    public void deposit(AccountType acctType, double amt, Integer employeeId) {
+        employee.deposit(acctType,amt,employeeId);
+
+    }
+
+    @Override
+    public boolean withdraw(AccountType acctType, double amt, Integer employeeId) {
+        boolean flag=false;
+        if(employee.withdraw(acctType,amt,employeeId))
+            flag=true;
+        else
+            System.out.println("Some error occurred");
+        return flag;
+    }
+
+    @Override
+    public String getFormattedAcctInfo(Integer employeeId) {
+        return employee.getFormattedAcctInfo(employeeId);
+    }
 }
